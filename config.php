fa26ab66e79fbeed17da9ec776108242b1cc0032
Bartosz Turecki <bartosz.turecki@gmail.com>
2022-05-22 20:36:56 +0200

    Connect to database, install pdo drivers, create user repository.

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+const USERNAME = 'mxcbqopgvfyrji';
+const PASSWORD = 'b927d28ae26229905f6c587d4948abeb3d8184840c2c36de0d6a6a5831948ad0';
+const HOST = 'ec2-52-30-67-143.eu-west-1.compute.amazonaws.com';
+const DATABASE = 'ddf6502eh5b62n';
